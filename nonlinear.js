26ed0898a62de527fdc65f0d59a1dfa50516c08d
Olive <golyo278@newschool.edu>
2018-02-22 16:27:37 -0500

    ok

---
+++ nonlinear.js
@@ -0,0 +1,100 @@
+$(".block2").click(function() {
+  $(".ftout").hide();
+  $(".blocks").hide();
+  $(".tile1").show();
+  $(".tile3").show();
+  
+});
+$(".block3").click(function() {
+ $(".ftout").hide();
+  $(".tile2").show();
+  $(".tile4").show();
+  
+});
+
+$(".block4").click(function() {
+ $(".ftout").hide();
+$(".tile5").show();
+  $(".tile2").show();
+  
+});
+
+$(".block5").click(function() {
+ $(".ftout").hide();
+  $(".tile1").show();
+  $(".tile7").show();
+  
+});
+
+$(".block6").click(function() {
+ $(".ftout").hide();
+  $(".tile8").show();
+  $(".tile3").show();
+  
+});
+
+$(".block7").click(function() {
+$(".ftout").hide();
+  $(".tile5").show();
+  $(".tile7").show();
+  
+});
+
+$(".block8").click(function() {
+  $(".ftout").hide();
+  $(".tile4").show();
+  $(".tile6").show();
+  
+});
+
+$(".block9").click(function() {
+ $(".ftout").hide();
+  $(".tile3").show();
+  $(".tile2").show();
+});
+
+$(".flip1").click(function() {
+ $(".tiles").hide();
+  $(".life1").show();
+});
+
+$(".flip2").click(function() {
+ $(".tiles").hide();
+  $(".life2").show();
+});
+
+$(".flip3").click(function() {
+ $(".tiles").hide();
+  $(".life3").show();
+});
+
+$(".flip4").click(function() {
+ $(".tiles").hide();
+  $(".life4").show();
+});
+
+$(".flip5").click(function() {
+ $(".tiles").hide();
+  $(".life5").show();
+});
+
+$(".flip6").click(function() {
+ $(".tiles").hide();
+  $(".life6").show();
+});
+
+$(".flip7").click(function() {
+ $(".tiles").hide();
+  $(".life7").show();
+});
+
+$(".flip8").click(function() {
+ $(".tiles").hide();
+  $(".life8").show();
+});
+
+$(".blocks").click(function() {
+ $(".fate").hide();
+ 
+});
+
