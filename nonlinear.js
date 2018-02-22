2bd675b8e603aa9c4cbfa0db10a930638367257b
Olive <golyo278@newschool.edu>
2018-02-22 16:50:18 -0500

    sos

--- nonlinear.js
+++ nonlinear.js
@@ -2,14 +2,3 @@ $(".fate").click(function() {
   $(".fate").hide();
   
 });
-
-$(".flip1").click(function() {
- $(".tiles").hide();
-  $(".life1").show();
-});
-
-document.getElementById("fate").onclick = function() {myFunction()};
-
-function myFunction() {
-    document.getElementById("fate").innerHTML = "YOU CLICKED ME!";
-}
