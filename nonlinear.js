639a5ded33b3016f40116fc1a34d6710ae273215
Olive <golyo278@newschool.edu>
2018-02-25 15:23:41 -0500

    update html text

---
+++ nonlinear.js
@@ -0,0 +1,36 @@
+$(".hide").click(function() {
+  $(".hide").hide(); 
+});
+
+$(".alert").click(function() {
+    alert("BOO!"); 
+});
+
+$(".color").click(function() {
+    $( this ).css( "background-color", "red" );
+});
+
+
+var audio = new Audio('Bandrew.m4a');
+$(".sound").click(function() {
+  audio.play(); 
+});
+
+$(".idk").click(function(){
+    $('.dog').toggle();
+});
+
+$(".animate").click(function(){
+    $(".animate").animate({
+        left: '250px',
+        opacity: '0.5',
+        height: '150px',
+        width: '150px'});
+});
+
+
+$(".fade").click(function(){
+     $(".fade").fadeTo(1000, 0.4);
+});
+
+
